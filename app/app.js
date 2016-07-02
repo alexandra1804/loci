1673dd7c608201442ba177d9b9287344b81b7d3c
alexandra1804 <alexandra.abramovich@gmail.com>
2016-07-02 14:27:57 +0300

    update

---
+++ app/app.js
@@ -0,0 +1,139 @@
+//Файл с ангуляровским кодом
+
+angular
+    .module('myApp', ['ngRoute', 'firebase'])
+    .constant('FirebaseUrl', 'https://myapps-ee62d.firebaseio.com/')
+    .service('firebaseRef', ['FirebaseUrl', Firebase])
+    .directive('markdown', linkConverter)
+    .controller('listCtrl', listController)
+    .controller('newCtrl', newController)
+    .config(ApplicationConfig);
+
+function ApplicationConfig($routeProvider) {
+    $routeProvider.when('/', {
+            controller: 'listCtrl',
+            templateUrl: 'views/appslist.html',
+            callback: console.log('view h b loaded')
+    })
+                  .when('/new', {
+            controller: 'newCtrl',
+            templateUrl: 'views/newapp.html',
+            callback: console.log('view h b loaded')
+        });
+}
+    
+function listController($scope, firebaseRef, $firebaseArray) {
+    $scope.load = function() {
+       $('.button-collapse').sideNav({
+           closeOnClick: true
+       });
+       $('.parallax').parallax();
+    };
+    
+    $scope.load();
+    
+    $scope.list = $firebaseArray(firebaseRef);
+    $scope.list.$loaded().then(function() {
+        console.log($scope.list);
+        console.log("The number of projects in DB is " + $scope.list.length);
+    }.bind($scope));
+    
+    $scope.search = {};
+    $scope.userInput = {};
+    
+    $scope.applySearch = function() {
+        for(name in $scope.userInput) {
+            $scope.search[name] = $scope.userInput[name];
+        }
+    };
+};
+
+function newController($scope, firebaseRef, $firebaseArray) {
+    $scope.list = $firebaseArray(firebaseRef);
+    $scope.nameInput = [];
+    $scope.descrInput = [];
+        $scope.addProject = function() {
+            $scope.newListItem = firebaseRef.push();
+            $scope.newListItem.set({
+                name: $scope.nameInput,
+                description: $scope.descrInput
+            });
+            console.log("A new project has been added");
+            console.log( $scope.nameInput);
+            console.log( $scope.descrInput);
+        };
+};
+
+function linkConverter() {
+    var converter = new Showdown.converter();
+    return {
+      scope: {
+        markdown: '@'
+      },
+      link: function(scope, element, attrs) {
+        scope.$watch('markdown', function() {
+          var content = converter.makeHtml(attrs.markdown);
+          element.html(content);
+        });
+      }
+    }
+};
+
+
+/*'use strict';
+angular
+    .module('myApp', ['ngRoute'])
+    .controller('listCtrl', MyController)
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+        .when('/', {
+            controller: 'listCtrl',
+            templateUrl: 'views/appslist.html'
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+    }]);
+
+function MyController ($scope) {
+    $scope.init = function ( ) {
+        $scope.rootRef = new Firebase('https://myapps-angfire.firebaseio.com/');
+        $scope.list = [];
+        $scope.rootRef.on('child_added', function (snapshot) {
+            $scope.message = snapshot.val();
+            //console.log($scope.message);
+            $scope.list.push($scope.message);
+            return $scope.list;
+        });   
+    };
+$scope.init();
+};
+*/
+/*
+$scope.init = function ( ) {
+    $scope.rootRef = new Firebase('https://myapps-angfire.firebaseio.com/');
+    $scope.rootRef.on('child_added', function (snapshot) {
+        $scope.
+        $scope.message = snapshot.val();
+        console.log($scope.message);
+    });   
+};
+$scope.init();
+*/
+
+/*var lblCurrentMessage = document.getElementById('lblCurrentMessage'),
+    txtNewMessage = document.getElementById('txtNewMessage'),
+    btUpdateMessage = document.getElementById('btUpdateMessage'),
+    rootRef = new Firebase('https://myapps-angfire.firebaseio.com/');
+    currentMessageRef = rootRef.child('currentMessage');
+
+
+btUpdateMessage.addEventListener('click', function() {
+    currentMessageRef.set(txtNewMessage.value);
+    txtNewMessage.value = '';
+});
+
+currentMessageRef.on('value', function(snapshot) {
+    lblCurrentMessage.innerText = snapshot.val();
+});
+*/
