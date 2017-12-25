de18d70ac2b7aea241086492b40ce5648cc493d0
alexandra_abramovich <aabramovich@exadel.com>
2017-12-25 23:46:14 +0300

    added example data generation :D

--- app.py
+++ app.py
@@ -22,6 +22,13 @@ def showList():
   jsonify({'result' : output})
   return render_template('list.html', notes=output)
 
+@app.route('/add-default', methods=['GET'])
+def add_default():
+  notes = mongo.db.notes
+  notes.insert({'label': 'About rabbits', 'descr': 'Rabbits are small mammals in the family Leporidae of the order Lagomorpha, found in several parts of the world. There are eight different genera in the family classified as rabbits, including the European rabbit (Oryctolagus cuniculus), cottontail rabbits (genus Sylvilagus; 13 species), and the Amami rabbit (Pentalagus furnessi, an endangered species on Amami Ōshima, Japan). There are many other species of rabbit, and these, along with pikas and hares, make up the order Lagomorpha. The male is called a buck and the female is a doe; a young rabbit is a kitten or kit.', 'link' : 'https://en.wikipedia.org/wiki/Rabbit'})
+  notes.insert({'label': 'Material Icons', 'descr': 'Source of icons used in UI deveopment provided by Google.', 'link' : 'https://material.io/icons/'})
+  return redirect('/')
+
 @app.route('/edit', methods=['GET'])
 def showEdit():
   id = request.args.get('id')
@@ -38,16 +45,16 @@ def showEdit():
 
 @app.route('/save', methods=['POST'])
 def save_note():
-    notes = mongo.db.notes
-    label = request.form['label']
-    descr = request.form['descr']
-    link = request.form['link']
-    id = request.args.get('id')
-    if id:
-      notes.update({'_id' : ObjectId(id)}, {'$set': {'label': label, 'descr': descr, 'link' : link}})
-    else:
-      notes.insert({'label': label, 'descr': descr, 'link' : link})
-    return redirect('/')
+  notes = mongo.db.notes
+  label = request.form['label']
+  descr = request.form['descr']
+  link = request.form['link']
+  id = request.args.get('id')
+  if id:
+    notes.update({'_id' : ObjectId(id)}, {'$set': {'label': label, 'descr': descr, 'link' : link}})
+  else:
+    notes.insert({'label': label, 'descr': descr, 'link' : link})
+  return redirect('/')
 
 @app.route('/delete', methods=['GET'])
 def deleteNote():
