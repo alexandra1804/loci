fe4454d3796f5f7c94357f79cd86121830964135
alexandra_abramovich <aabramovich@exadel.com>
2017-12-25 18:23:03 +0300

    implemented submitting new entry, removed search for now

--- app.py
+++ app.py
@@ -1,48 +1,60 @@
-from flask import Flask,render_template
-from flask import jsonify
-from flask import request
+from flask import Flask,render_template,flash,redirect,request,jsonify
+from bson import ObjectId
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+# User needs to have Mondo database called 'memodb'
+# TODO: add scripts for clean start and migration
 
 app.config['MONGO_DBNAME'] = 'memodb'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/memodb'
 
 mongo = PyMongo(app)
 
-def renderEditPage(note):
-	return render_template('edit.html', note)
-
 @app.route('/', methods=['GET'])
 def showList():
   notes = mongo.db.notes
   output = []
   for note in notes.find():
-	  output.append({'label' : note['label'], 'descr' : note['descr']})
+    id = str(note['_id'])
+    output.append({'id' : id, 'label' : note['label'], 'descr' : note['descr']})
   jsonify({'result' : output})
   return render_template('list.html', notes=output)
 
 @app.route('/edit', methods=['GET'])
-def showEdit(label):
-  notes = mongo.db.notes
-  note = notes.find_one({'label' : label})
-  if note:
-    output = {'label' : note['label'], 'descr' : note['descr']}
-  else:
-    output = "No such note"
+def showEdit():
+  id = request.args.get('id')
+  output = []
+  if id:
+    oid = ObjectId(id)
+    notes = mongo.db.notes
+    note = notes.find_one({'_id' : ObjectId(id)})
+    if note:
+      output = {'label' : note['label'], 'descr' : note['descr']}
+    else:
+      output = "No such note"
   jsonify({'result' : output})
-  return renderEditPage(note=output)
+  return render_template('edit.html', note=output)
 
-@app.route('/edit', methods=['POST'])
+# @app.route('/edit', methods=['POST'])
+# def add_note():
+#   notes = mongo.db.notes
+#   label = request.json['label']
+#   descr = request.json['descr']
+#   note_id = notes.insert({'label': label, 'descr': descr})
+#   new_note = notes.find_one({'_id': note_id })
+#   output = {'label' : new_star['label'], 'descr' : new_star['descr']}
+#   jsonify({'result' : output})
+#   showList()
+
+@app.route('/add', methods=['POST'])
 def add_note():
-  notes = mongo.db.notes
-  label = request.json['label']
-  descr = request.json['descr']
-  note_id = notes.insert({'label': label, 'descr': descr})
-  new_note = notes.find_one({'_id': note_id })
-  output = {'label' : new_star['label'], 'descr' : new_star['descr']}
-  jsonify({'result' : output})
-  showList()
+    notes = mongo.db.notes
+    label = request.form['label']
+    descr = request.form['descr']
+    notes.insert({'label': label, 'descr': descr})
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/helpers/init.js
+++ static/js/helpers/init.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
   $('.parallax').parallax();
+  $('.button-collapse').sideNav({
+    menuWidth: 300,
+    edge: 'left',
+    closeOnClick: true,
+    draggable: true
+  });
 });
